dfbe164937995822da9cbeca92c2f2e25bf1a18b
Ekter <ektermoi1@gmail.com>
2024-08-18 23:10:09 +0200

    feat: bug fix

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import ftprci as fci
 
 # Create an instance of the LSM6 sensor
-sensor = fci.LSM6()
+sensor = fci.LSM6(0x6B)
 
-# Create an instance of the PIDController
+estimator = fci.ComplementaryFilter()
 
+pid = fci.PIDController(10, 0, 3, fci.DiscreteIntegral.Tustin())
+
+motors = fci.PololuAstar()
 
 th = fci.RunnerThread()
 
-th.callback | sensor.read
+th.callback | sensor.read | estimator.estimate | pid.steer | motors.command
 
 th.run()
